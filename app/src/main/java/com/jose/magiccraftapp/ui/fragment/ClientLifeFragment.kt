43e361c68c9741b336f3d079fb35f857359f14e2
JoseJimenezGonzalez <josexisco1993@gmail.com>
2024-06-03 16:42:34 +0200

    commit ok

--- app/src/main/java/com/jose/magiccraftapp/ui/fragment/ClientLifeFragment.kt
+++ app/src/main/java/com/jose/magiccraftapp/ui/fragment/ClientLifeFragment.kt
@@ -6,7 +6,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.jose.magiccraftapp.databinding.FragmentClientLifeBinding
-import java.util.concurrent.Semaphore
 
 
 class ClientLifeFragment : Fragment() {
@@ -19,7 +18,6 @@ class ClientLifeFragment : Fragment() {
 
     var lifeOther = 20
 
-    private val semaphore = Semaphore(1)
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -40,23 +38,17 @@ class ClientLifeFragment : Fragment() {
         binding.tvLifeOther.text = lifeOther.toString()
         //Se le da a menos
         binding.ivMinusOther.setOnClickListener {
-            semaphore.acquire()
             lifeOther--
             binding.tvLifeOther.text = lifeOther.toString()
-            semaphore.release()
         }
         //Se le da a mas
         binding.ivPlusOther.setOnClickListener {
-            semaphore.acquire()
             lifeOther++
             binding.tvLifeOther.text = lifeOther.toString()
-            semaphore.release()
         }
         binding.ivAutorenewOther.setOnClickListener {
-            semaphore.acquire()
             lifeOther = 20
             binding.tvLifeOther.text = lifeOther.toString()
-            semaphore.release()
         }
     }
 
@@ -64,23 +56,17 @@ class ClientLifeFragment : Fragment() {
         binding.tvLifeYou.text = lifeYou.toString()
         //Se le da a menos
         binding.ivMinusYou.setOnClickListener {
-            semaphore.acquire()
             lifeYou--
             binding.tvLifeYou.text = lifeYou.toString()
-            semaphore.release()
         }
         //Se le da a mas
         binding.ivPlusYou.setOnClickListener {
-            semaphore.acquire()
             lifeYou++
             binding.tvLifeYou.text = lifeYou.toString()
-            semaphore.release()
         }
         binding.ivAutorenewYou.setOnClickListener {
-            semaphore.acquire()
             lifeYou = 20
             binding.tvLifeYou.text = lifeYou.toString()
-            semaphore.release()
         }
     }
 
